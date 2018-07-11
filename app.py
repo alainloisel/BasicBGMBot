4a45b75e1adcdf8037dfa80a2a0a7d1120f8e0e1
Alain <alainloisel@gmail.com>
2018-07-11 13:13:49 +0200

    environ

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from pymessenger.bot import Bot
 import os 
 app = Flask(__name__)
 ACCESS_TOKEN = 'ACCESS_TOKEN'   #
-#ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 VERIFY_TOKEN = 'VERIFY_TOKEN'   
-#VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
+VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
 bot = Bot (ACCESS_TOKEN)
 
 @app.route('/politique')
